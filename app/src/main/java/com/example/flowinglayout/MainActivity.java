bd09e93dbbe0abfbd836b2d904dc609e8d8c5720
chents <xx@qq.com>
2021-05-17 21:48:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/flowinglayout/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.flowinglayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cts.flow.CommonFlowAdapter;
+import com.cts.flow.FlowingLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FlowingLayout mFlow;
+    private List<String> list = new ArrayList<>();
+    CommonFlowAdapter commonFlowAdapter=    new CommonFlowAdapter<String>(this){
+
+        @Override
+        public String getItem(int i) {
+            return mDatas.get(i);
+        }
+
+        @Override
+        public void convert(FlowHolder holder, String item, int position) {
+
+            holder.setText(R.id.title,item);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+        list.add("111");
+        list.add("2");
+        list.add("3344444444433");
+        list.add("YYYYYYYY");
+        list.add("EEEEE");
+        list.add("FFFFFFFF");
+
+        mFlow = (FlowingLayout) findViewById(R.id.flow);
+        mFlow.setChildLRMargin(10);
+        mFlow.setChildTBMargin(5);
+        mFlow.setPadding(15,5,15,5);
+        mFlow.setBorder("#DAE0DC",1, Color.RED,15);
+        mFlow.setChildTextColor(Color.parseColor("#262E79"));
+
+        mFlow.setAdapter(commonFlowAdapter);
+
+        commonFlowAdapter.addItems(list);
+
+        commonFlowAdapter.notifyDataSetChanged();
+
+    }
+}
