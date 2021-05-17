a3ce0b3ccd10360413f4373334c69516994c2cca
chents <xx@qq.com>
2021-05-17 22:30:32 +0800

    test 远程仓库的使用

--- app/src/main/java/com/example/flowinglayout/MainActivity.java
+++ app/src/main/java/com/example/flowinglayout/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import com.cts.flow.CommonFlowAdapter;
 import com.cts.flow.FlowingLayout;
 
@@ -33,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+
     }
 
     private void initView() {
